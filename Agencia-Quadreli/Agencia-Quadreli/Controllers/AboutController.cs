f8491151707d789b3e31ea0a302ab02c02029f0c
Aziz Cotrim <azizcotrim8@gmail.com>
2025-06-12 14:44:14 -0300

    Exclusão da pagina Sobre

--- Agencia-Quadreli/Agencia-Quadreli/Controllers/AboutController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Agencia_Quadreli.Controllers {
-    public class AboutController : Controller {
-        public IActionResult Index() {
-            return View();
-        }
-    }
-}
